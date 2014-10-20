7f29fafde724fc266e8832af439cd9ea19aca594
Oisin Mulvihill <oisin.mulvihill@gmail.com>
2014-10-20 17:46:24 +0100

    ditching pkglib as its causing more pain then usefulness to me.

--- setup.py
+++ setup.py
@@ -1,12 +1,70 @@
 # -*- coding: utf-8 -*-
 """
-Setuptools script for pp-metrics (pp.metrics)
 """
-try:
-    from pkglib.setuptools import setup
-except ImportError:
-    print "PkgLib is not available. Please run \"easy_install pkglib\""
-    import sys
-    sys.exit(1)
+from setuptools import setup, find_packages
 
-setup()
+Name = "pp-metrics"
+ProjectUrl = ""
+Version = "1.0.0"
+Author = 'Oisin Mulvihill'
+AuthorEmail = 'oisin mulvihill at gmail'
+Maintainer = ''
+Summary = 'Scales metrics integration helper for pp-web-base apps.'
+License = ''
+Description = Summary
+ShortDescription = Summary
+
+
+needed = [
+    "scales",
+]
+
+test_needed = [
+    "pytest-cov",
+]
+
+test_suite = ''
+
+EagerResources = [
+    'pp',
+]
+
+ProjectScripts = [
+]
+
+PackageData = {
+    '': ['*.*'],
+}
+
+
+# [console] ??
+#     testing = pp.testing.scripts.app:main
+EntryPoints = """
+"""
+
+setup(
+    url=ProjectUrl,
+    name=Name,
+    zip_safe=False,
+    version=Version,
+    author=Author,
+    author_email=AuthorEmail,
+    description=ShortDescription,
+    long_description=Description,
+    classifiers=[
+        "Topic :: Software Development :: Libraries",
+        "Programming Language :: Python",
+    ],
+    keywords='python',
+    license=License,
+    scripts=ProjectScripts,
+    install_requires=needed,
+    tests_require=test_needed,
+    test_suite=test_suite,
+    include_package_data=True,
+    packages=find_packages(),
+    package_data=PackageData,
+    eager_resources=EagerResources,
+    entry_points=EntryPoints,
+    namespace_packages=['pp'],
+)
